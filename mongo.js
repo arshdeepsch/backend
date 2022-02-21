d866af8ba52ebbf761d7ffdedfd33ff5e99980b8
Arshdeep Chudda <arshdeepsch@gmail.com>
2022-02-21 13:01:20 -0500

    finished 3.12

--- mongo.js
+++ mongo.js
@@ -1,32 +1,56 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+if (process.argv.length == 5) {
+
+
+  const password = process.argv[2];
+
+  const url = `mongodb+srv://fullstack:${password}@phonebook-1.sympi.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+  mongoose.connect(url);
+  const phonebookSchema = new mongoose.Schema({
+    name: String,
+    date: Date,
+    number: String,
+  })
+
+  const phoneModel = mongoose.model('Number', phonebookSchema);
+
+  const number = new phoneModel({
+    name: process.argv[3],
+    date: new Date(),
+    number: process.argv[4],
+  })
+
+  number.save().then(
+    result => {
+      console.log(`added ${result.name} number ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
+
+} else if (process.argv.length == 3) {
+
+  const password = process.argv[2];
+
+  const url = `mongodb+srv://fullstack:${password}@phonebook-1.sympi.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+  mongoose.connect(url);
+  const phonebookSchema = new mongoose.Schema({
+    name: String,
+    date: Date,
+    number: String,
+  })
+
+  const phoneModel = mongoose.model('Number', phonebookSchema);
+  phoneModel.find({}).then(
+    result => {
+      result.forEach(number => {
+        console.log(`${number.name} ${number.number}`);
+      })
+      mongoose.connection.close()
+    }
+  )
+
+} else {
+  console.log('format the command as  node mongo.js <password> <name> <number>');
 }
-
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.zsw5l.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
