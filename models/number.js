6d7e79ff8f8406427707b490c2cfa1f223c43216
Arshdeep Chudda <arshdeepsch@gmail.com>
2022-03-13 20:50:28 -0400

    finished 3.22
    added eslint

--- models/number.js
+++ models/number.js
@@ -4,42 +4,42 @@ const uri = process.env.MONGODB_URI
 
 console.log('connecting to ', uri)
 
-mongoose.connect(uri).then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+mongoose.connect(uri).then((result) => {
+  console.log('connected to MongoDB')
+})
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const numberSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 10,
-        required: true,
-        validate: {
-            validator: (v) => {
-                return /\d{3}-\d{3}-\d{4}/.test(v)
-            },
-            message: (props) => `${props.value} is not a valid phone number!`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 10,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /\d{3}-\d{3}-\d{4}/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`
     }
+  }
 })
 
 numberSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Number', numberSchema)
--- mongo.js
+++ mongo.js
@@ -1,55 +1,55 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-if (process.argv.length != 5 && process.argv.length != 3) {
-  console.log('proper form: node mongo.js <passwod> <name> <number>')
-} else if (process.argv.length == 5) {
-  const pass = process.argv[2]
-  const uri = `mongodb://fullstack:${pass}@phonebook-shard-00-00.sympi.mongodb.net:27017,phonebook-shard-00-01.sympi.mongodb.net:27017,phonebook-shard-00-02.sympi.mongodb.net:27017/Phonebook?ssl=true&replicaSet=atlas-5i41jf-shard-0&authSource=admin&retryWrites=true&w=majority`
+// if (process.argv.length != 5 && process.argv.length != 3) {
+//   console.log('proper form: node mongo.js <passwod> <name> <number>')
+// } else if (process.argv.length == 5) {
+//   const pass = process.argv[2]
+//   const uri = `mongodb://fullstack:${pass}@phonebook-shard-00-00.sympi.mongodb.net:27017,phonebook-shard-00-01.sympi.mongodb.net:27017,phonebook-shard-00-02.sympi.mongodb.net:27017/Phonebook?ssl=true&replicaSet=atlas-5i41jf-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-  mongoose.connect(uri)
+//   mongoose.connect(uri)
 
-  const numberSchema = new mongoose.Schema({
-    name: String,
-    date: Date,
-    number: String,
-  })
+//   const numberSchema = new mongoose.Schema({
+//     name: String,
+//     date: Date,
+//     number: String,
+//   })
 
-  const Number = mongoose.model('Number', numberSchema)
+//   const Number = mongoose.model('Number', numberSchema)
 
-  const number = new Number({
-    name: process.argv[3],
-    date: new Date(),
-    number: process.argv[4]
-  })
+//   const number = new Number({
+//     name: process.argv[3],
+//     date: new Date(),
+//     number: process.argv[4]
+//   })
 
-  number.save().then(result => {
-    console.log(`added ${result.name} ${result.number} to phonebook`)
-    mongoose.connection.close()
-  })
-} else if (process.argv.length == 3) {
-  const pass = process.argv[2]
-  const uri = `mongodb://fullstack:${pass}@phonebook-shard-00-00.sympi.mongodb.net:27017,phonebook-shard-00-01.sympi.mongodb.net:27017,phonebook-shard-00-02.sympi.mongodb.net:27017/Phonebook?ssl=true&replicaSet=atlas-5i41jf-shard-0&authSource=admin&retryWrites=true&w=majority`
+//   number.save().then(result => {
+//     console.log(`added ${result.name} ${result.number} to phonebook`)
+//     mongoose.connection.close()
+//   })
+// } else if (process.argv.length == 3) {
+//   const pass = process.argv[2]
+//   const uri = `mongodb://fullstack:${pass}@phonebook-shard-00-00.sympi.mongodb.net:27017,phonebook-shard-00-01.sympi.mongodb.net:27017,phonebook-shard-00-02.sympi.mongodb.net:27017/Phonebook?ssl=true&replicaSet=atlas-5i41jf-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-  mongoose.connect(uri)
+//   mongoose.connect(uri)
 
-  const numberSchema = new mongoose.Schema({
-    name: String,
-    date: Date,
-    number: String,
-  })
+//   const numberSchema = new mongoose.Schema({
+//     name: String,
+//     date: Date,
+//     number: String,
+//   })
 
-  const Number = mongoose.model('Number', numberSchema)
+//   const Number = mongoose.model('Number', numberSchema)
 
-  const number = new Number({
-    name: process.argv[3],
-    date: new Date(),
-    number: process.argv[4]
-  })
+//   const number = new Number({
+//     name: process.argv[3],
+//     date: new Date(),
+//     number: process.argv[4]
+//   })
 
-  Number.find({}).then(result => {
-    result.forEach(number => {
-      console.log(`${number.name} ${number.number}`)
-    });
-    mongoose.connection.close()
-  })
-}
+//   Number.find({}).then(result => {
+//     result.forEach(number => {
+//       console.log(`${number.name} ${number.number}`)
+//     });
+//     mongoose.connection.close()
+//   })
+// }
